b9f1f2c33d2f6df4813fb87484f5a2a22f489c04
batonia <51698746+batonia@users.noreply.github.com>
2023-04-12 22:41:05 +0700

    data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pymysql
 import pandas as pd
 
 # Define the API URL and parameters
@@ -10,22 +11,26 @@ response = requests.get(url)
 
 # Check the status code
 if response.status_code == 200:
-    # Extract the data from the response
+    # Extract the raw data from the response
     data = response.json()
 
     json_data = json.dumps(data, indent=4)
-
-    json_obj = json.loads(json_data)
-
-    del json_obj['source']
-
-    json_data = json.dumps(json_obj, indent=4)
-
-    with open("data.json", "w") as f:
+    with open("raw.json", "w") as f:
         f.write(json_data)
 
+    # Remove the source key from the json object
+    json_obj = json.loads(json_data)
+    del json_obj['source']
+
     # Create a pandas DataFrame from the data
-    df = pd.read_json('data.json')
-    print(df.to_string())  
+    json_data = json.dumps(json_obj, indent=4)
+    df = pd.read_json(json_data)
+    df.reset_index(drop=True, inplace=True)
+    with open("data.json", "w") as f:
+        f.write(df.to_string())  
+
+    # Connect to the database
+    cnt = pymysql.connect(host="localhost", user="root", password="", db="json")
+    cursor = cnt.cursor()
 else:
     print(response)
